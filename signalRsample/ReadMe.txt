0bca0bbe17178b0d84b2513a5146e06d13f70812
candy82202 <candy82202@yahoo.com.tw>
2023-09-07 11:49:43 +0800

    加入專案檔案。

---
+++ signalRsample/ReadMe.txt
@@ -0,0 +1,13 @@
+﻿[V]- 建立 SignalR Hub
+
+[V]- 建立 SignalR Hub
+
+[V]- 加入客戶端到SignalR
+
+[V]- 從客戶端的JS連到SignalR
+
+[V]- 呼叫SignalR的方法
+
+[V]- SignalR invoked 方法在Client JS裡面去通知顧客
+
+[V]- 客戶收到來自SignalR的更新跟動作。
---
+++ signalRsample/wwwroot/js/userCount.js
@@ -0,0 +1,31 @@
+﻿
+//建立連線
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount", signalR.HtttpTransport).build();
+
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+
+});
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+
+});
+function newWindowLoadedOnColint() {
+    //這裡要用invoke不然抓不到值，會顯示undefied
+    connectionUserCount.invoke("NewWindowLoaded").then((value)=> console.log(value));
+}
+
+
+//開始連線
+function fulfilled() {
+    console.log("連線成功")
+    newWindowLoadedOnColint();
+}
+
+function rejected() {
+//rejected logs
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
